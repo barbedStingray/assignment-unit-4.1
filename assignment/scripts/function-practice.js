637997a704bdf38e52e2d4a3e8487b8e59ecf99b
Ben Rehmann <brehmann@me.com>
2023-08-29 14:21:05 -0500

    need help

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,48 +14,81 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+
 function helloName(name) {
-  return;
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
+console.log( helloName('ben') );
+
+
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secNumber) {
   // return firstNumber + secondNumber;
+  return firstNumber + secNumber;
 }
-
+console.log( addNumbers(12, 18));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(num0, num1, num2) {
+  return num0 * num1 * num2;
 }
+console.log(multiplyThree(2,3,4));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
-  }
-  return;
+    return true
+  } else
+  return false
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
+console.log(isPositive(3));
+console.log(isPositive(-1));
+console.log(isPositive(0));
+
+
+
+
+
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  return (array.indexOf(array.length - 1));
 }
+
+
+
+
+
+
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
+  for(let i = 0; i < array.length; i++){
+    if( array[i] = value ){
+      return true
+    }//end of true
+  }// end of loop
+  return false;
+} // end of function
 
-}
+
+
+
+
+
+
 
 // ----------------------
 // Stretch Goals
